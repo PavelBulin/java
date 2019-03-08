b47e76fc2f38d80acb7bb485f2c4bb89bc11f787
bulinp <energ0watt@yandex.ru>
2019-03-08 08:56:05 +0300

    Was righted exeption 'arrayIndexOutOfBounds' and logic of the class filled on all array

--- AI.java
+++ AI.java
@@ -13,53 +13,62 @@ class AI {
 
 		
     public static void main(String[] args) {
-		int ind, di, dj, x, y;
+		int di, dj, x, y, inv;
+		double ind;
 		
 		for (int i = 0; i < 2; i++) {
 			for (int j = 0; j < 2; j++) {
-				di = i * (siz - 1));
-				dj = j * (siz - 1));
-				ind = mas[di][dj]
+				inv = i * (-2) + 1;
+				di = i * (siz - 1);
+				dj = j * (siz - 1);
+				ind = mas[di][dj];
 				if (mas[di][dj] != 0) { 
 					if (m[di][dj] == 1) {
 						if (i == j) { 
-							ind = ind + 1;
+							ind = ind + 1 * inv;
 							x = (int)Math.ceil(ind / siz);
-							y = ind - x * siz + siz;
-							if (m[x][y] == 0) {
+							y = (int)(ind - x * siz + siz);
+							if (m[--x][--y] == 0) {
 								m[x][y] = 5;
+								break;
 							} 
-							ind = ind + siz - 1;
+							ind = ind + (siz - 1) * inv;
 							x = (int)Math.ceil(ind / siz);
-							y = ind - x * siz + siz;
-							if (m[x][y] == 0) {
+							y = (int)(ind - x * siz + siz);
+							if (m[--x][--y] == 0) {
 								m[x][y] = 5;
+								break;
 							} 
-							ind = ind + 1;
+							ind = ind + 1 * inv;
 							x = (int)Math.ceil(ind / siz);
-							y = ind - x * siz + siz;
-							if (m[x][y] == 0) {
+							y = (int)(ind - x * siz + siz);
+							if (m[--x][--y] == 0) {
 								m[x][y] = 5;
+								break;
 							}
 							
 						} else if (i != j) { 
-							ind = ind + 1;
+							ind = ind - 1 * inv;
 							x = (int)Math.ceil(ind / siz);
-							y = ind - x * siz + siz;
-							if (m[x][y] == 0) {
+							y = (int)(ind - x * siz + siz);
+							if (m[--x][--y] == 0) {
 								m[x][y] = 5;
+								break;
 							} 
-							ind = ind + siz - 1;
+							ind = ind + (siz + 1) * inv;
 							x = (int)Math.ceil(ind / siz);
-							y = ind - x * siz + siz;
-							if (m[x][y] == 0) {
+							y = (int)(ind - x * siz + siz);
+							if (m[--x][--y] == 0) {
 								m[x][y] = 5;
+								break;
 							} 
-							ind = ind + 1;
+							ind = ind - 1 * inv;
 							x = (int)Math.ceil(ind / siz);
-							y = ind - x * siz + siz;
-							if (m[x][y] == 0) {
-								m[x][y] = 5;
+							y = (int)(ind - x * siz + siz);
+							if (m[--x][--y] == 0) {
+								m[x][y]
+								= 5;
+								break;
 							}
 						}
 						mas[di][dj] = 0;
@@ -69,7 +78,6 @@ class AI {
 		}
 	}
 }
-
 
 
 
