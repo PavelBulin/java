4c57de6313d0a41d018a806a9593628fe38c6821
bulinp <energ0watt@yandex.ru>
2019-03-22 15:54:18 +0300

    Was made parallel pass on the  rows and columns for the all typical cases

--- AII.java
+++ AII.java
@@ -2,7 +2,7 @@
 * Java Level 1. AII
 *
 *@author Pavel Bulin
-*@version 03/13/2019 
+*@version 03/22/2019 
 *@link https://github.com/PavelBulin/java.git
 */
 import java.util.*;
@@ -14,71 +14,47 @@ class AII {
 
 		
     public static void main(String[] args) {
-		int di, dj, x, y, inv;
+		int di, dj, x, y, inv, var;
 		double ind;
 		
 		for (int i = 0; i < 2; i++) {
+			di = i * (siz - 1);
 			for (int j = 1; j < siz - 1; j++) {
-				inv = -i * 2 + 1;
-				di = i * (siz - 1);
+				var = siz;
 				if (mas[di][j] != 0) { 
-					if (m[di][j] == 1) {
-						ind = mas[di][j] - 1 * inv;
-						x = (int)Math.ceil(ind / siz);
-						y = (int)(ind - x * siz + siz);
-						if (m[--x][--y] == 0) {
-							m[x][y] = 5;
-							break;
-						} 
-						ind = mas[di][j] + 1 * inv;
-						x = (int)Math.ceil(ind / siz);
-						y = (int)(ind - x * siz + siz);
-						if (m[--x][--y] == 0) {
-							m[x][y] = 5;
-							break;
-						} 
-						ind = mas[di][j] + siz * inv;
-						x = (int)Math.ceil(ind / siz);
-						y = (int)(ind - x * siz + siz);
-						if (m[--x][--y] == 0) {
-							m[x][y] = 5;
-							break;
+					for (int k = 0; k < 2; k++) {
+						inv = -i * 2 + 1;
+						var = (var == 1)? siz : 1;
+						if (m[di][j] == 1) {
+							ind = mas[di][j] - siz/siz * var * inv;
+							x = (int)ceil(ind / siz);
+							y = (int)(ind - x * siz + siz);
+							if (m[--x][--y] == 0) {
+								m[x][y] = 5;
+								mas[di][j] = 0;
+								break;
+							} 
+							ind = mas[di][j] + siz/siz * var * inv;
+							x = (int)ceil(ind / siz);
+							y = (int)(ind - x * siz + siz);
+							if (m[--x][--y] == 0) {
+								m[x][y] = 5;
+								mas[di][j] = 0;
+								break;
+							} 
+							ind = mas[di][j] + siz/var * inv;
+							x = (int)ceil(ind / siz);
+							y = (int)(ind - x * siz + siz);
+							if (m[--x][--y] == 0) {
+								m[x][y] = 5;
+								mas[di][j] = 0;
+								break;
+							}
 						}
-						mas[di][j] = 0;
+						di = j ^ di;
+						j = di ^ j;
+						di = j ^ di;
 					}
-				}
-			}
-		}
-		
-		for (int j = 0; j < 2; j++) {
-			for (int i = 1; i < siz - 1; i++) {
-				inv = j * (-2) + 1;
-				dj = j * (siz - 1);
-				if (mas[i][dj] != 0) { 
-					if (m[i][dj] == 1) {
-						ind = mas[i][dj] - siz * inv;
-						x = (int)Math.ceil(ind / siz);
-						y = (int)(ind - x * siz + siz);
-						if (m[--x][--y] == 0) {
-							m[x][y] = 5;
-							break;
-						} 
-						ind = mas[i][dj] + siz * inv;
-						x = (int)Math.ceil(ind / siz);
-						y = (int)(ind - x * siz + siz);
-						if (m[--x][--y] == 0) {
-							m[x][y] = 5;
-							break;
-						} 
-						ind = mas[i][dj] + 1 * inv;
-						x = (int)Math.ceil(ind / siz);
-						y = (int)(ind - x * siz + siz);
-						if (m[--x][--y] == 0) {
-							m[x][y] = 5;
-							break;
-						}
-					}
-					mas[i][dj] = 0;
 				}
 			}
 		}
